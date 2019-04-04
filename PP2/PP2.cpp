618f032ac22495f0764bde22b38203c353c4d4eb
Федякова Анастасия <fedyakova.anastasia@gmail.com>
2019-04-04 23:21:55 +0300

    TODO в PP2.cpp: реализовала создание необходимого количества экземпляров класса CBankClient.

--- PP2/PP2.cpp
+++ PP2/PP2.cpp
@@ -2,11 +2,21 @@
 #include "Bank.h"
 #include "BankClient.h"
 
-int main()
+int main(int argc, char *argv[])
 {
 	CBank* bank = new CBank();
-	CBankClient* client1 = bank->CreateClient();
-	CBankClient* client2 = bank->CreateClient();
+	//CBankClient* client1 = bank->CreateClient();
+	//CBankClient* client2 = bank->CreateClient();
+
+	int classInstancesNumber = 2;
+	if (argc == 2) {
+		int strToInt = atoi(argv[1]);
+		classInstancesNumber = strToInt;
+	}
+
+	for (int i = 0; i < classInstancesNumber; i++) {
+		CBankClient* client = bank->CreateClient();
+	}
 
 	// TODO: WaitForMultipleObjects
 	while (true)
