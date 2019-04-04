a1a6654c0400ff95df3aba6fb1ecfd343ea70752
Федякова Анастасия <fedyakova.anastasia@gmail.com>
2019-04-04 23:31:19 +0300

    TODO в PP2.cpp: реализовала условие выхода из программы при вводе с клавиатуры ключевой фразы.

--- PP2/PP2.cpp
+++ PP2/PP2.cpp
@@ -1,27 +1,37 @@
 #include "stdafx.h"
 #include "Bank.h"
 #include "BankClient.h"
+#include "iostream"
+#include "string"
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
 	CBank* bank = new CBank();
 	//CBankClient* client1 = bank->CreateClient();
 	//CBankClient* client2 = bank->CreateClient();
 
 	int classInstancesNumber = 2;
-	if (argc == 2) {
+	if (argc == 2)
+	{
 		int strToInt = atoi(argv[1]);
 		classInstancesNumber = strToInt;
 	}
 
-	for (int i = 0; i < classInstancesNumber; i++) {
+	for (int i = 0; i < classInstancesNumber; i++)
+	{
 		CBankClient* client = bank->CreateClient();
 	}
 
 	// TODO: WaitForMultipleObjects
 	while (true)
 	{
+		std::string exit;
+		std::cin >> exit;
+		if (exit == "exit" || exit == "quit")
+		{
+			break;
+		}
 	}
 
-    return 0;
+	return 0;
 }
